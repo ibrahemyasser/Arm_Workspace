b822e42e118c2b90e895e95438a75f90d7a7041b
ibrahemyasser <ebrahim195016@feng.bu.edu.eg>
2022-08-21 15:38:15 +0200

    DIO added

--- SrcCode/Common/Mcu_Hw.h
+++ SrcCode/Common/Mcu_Hw.h
@@ -43,14 +43,76 @@ typedef union
     INTCTRL_BF B;
 }INTCTRL_Tag;
 
+/*********************** Register map for GPIO ***********************/
+typedef struct
+{
+	volatile const uint32_t RESERVED[255];
+	volatile uint32_t 		GPIODATA;
+	volatile uint32_t 		GPIODIR;
+	volatile uint32_t 		GPIOIS;
+	volatile uint32_t 		GPIOIBE;
+	volatile uint32_t 		GPIOIEV;
+	volatile uint32_t 		GPIOIM;
+	volatile uint32_t 		GPIORIS;
+	volatile uint32_t 		GPIOMIS;
+	volatile uint32_t 		GPIOICR;
+	volatile uint32_t 		GPIOAFSEL;
+	volatile uint32_t 		RESERVED1[55];
+	volatile uint32_t 		GPIODR2R;
+	volatile uint32_t 		GPIOR4R;
+	volatile uint32_t 		GPIOR8R;
+	volatile uint32_t 		GPIOODR;
+	volatile uint32_t 		GPIOPUR;
+	volatile uint32_t 		GPIOPDR;
+	volatile uint32_t 		GPIOSLR;
+	volatile uint32_t 		GPIODEN;
+	volatile uint32_t 		GPIOLOCK;
+	volatile uint32_t 		GPIOCR;
+	volatile uint32_t 		GPIOAMSEL;
+	volatile uint32_t 		GPIOPCTL;
+	volatile uint32_t 		GPIOADCCTL;
+	volatile uint32_t 		GPIOADMACTL;
+}GPIO_Type;
+
 
 
 /**********************************************************************************************************************
  *  GLOBAL CONSTANT MACROS
  *********************************************************************************************************************/
-#define CORTEXM4_PERI_BASE_ADDRESS             0xE000E000
-#define APINT                                  *((volatile uint32*)(CORTEXM4_PERI_BASE_ADDRESS+0xD0C))
-#define INTCTRL                                *((volatile INTCTRL_Tag*)(CORTEXM4_PERI_BASE_ADDRESS+0xD04))
+#define CORTEXM4_PERI_BASE_ADDRESS             	0xE000E000
+#define APINT                                  	*((volatile uint32*)(CORTEXM4_PERI_BASE_ADDRESS+0xD0C))
+#define INTCTRL                                	*((volatile INTCTRL_Tag*)(CORTEXM4_PERI_BASE_ADDRESS+0xD04))
+
+/*********************** GPIO base address ***********************/
+#define GPIOA_BASE							   	0x40004000UL
+#define GPIOA_BASE_AHB						   	0x40058000UL
+#define GPIOB_BASE							   	0x40005000UL
+#define GPIOB_BASE_AHB							0x40059000UL
+#define GPIOC_BASE							  	0x40006000UL
+#define GPIOC_BASE_AHB							0x4005A000UL
+#define GPIOD_BASE							   	0x40007000UL
+#define GPIOD_BASE_AHB						   	0x4005B000UL
+#define GPIOE_BASE							   	0x40024000UL
+#define GPIOE_BASE_AHB							0x4005C000UL
+#define GPIOF_BASE							  	0x40025000UL
+#define GPIOF_BASE_AHB							0x4005D000UL
+
+
+/*********************** Peripheral Declaration ***********************/
+
+#define GPIOA									((GPIO_Type		*)	GPIOA_BASE)
+#define GPIOA_AHB								((GPIO_Type		*)	GPIOA_BASE_AHB)
+#define GPIOB									((GPIO_Type		*)	GPIOB_BASE)
+#define GPIOB_AHB								((GPIO_Type		*)	GPIOB_BASE_AHB)
+#define GPIOC									((GPIO_Type		*)	GPIOC_BASE)
+#define GPIOC_AHB								((GPIO_Type		*)	GPIOC_BASE_AHB)
+#define GPIOD									((GPIO_Type		*)	GPIOD_BASE)
+#define GPIOD_AHB								((GPIO_Type		*)	GPIOD_BASE_AHB)
+#define GPIOE									((GPIO_Type		*)	GPIOE_BASE)
+#define GPIOE_AHB								((GPIO_Type		*)	GPIOE_BASE_AHB)
+#define GPIOF									((GPIO_Type		*)	GPIOF_BASE)
+#define GPIOF_AHB								((GPIO_Type		*)	GPIOF_BASE_AHB)
+
 
 /**********************************************************************************************************************
  *  GLOBAL DATA PROTOTYPES
